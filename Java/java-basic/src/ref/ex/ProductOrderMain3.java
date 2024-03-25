97617606087c142d390de4255dfcd5664eb15f6b
baechanghyeon12 <qo99033@naver.com>
2024-03-25 22:23:23 +0900

    Java method

--- Java/java-basic/src/ref/ex/ProductOrderMain3.java
+++ Java/java-basic/src/ref/ex/ProductOrderMain3.java
@@ -1,13 +1,29 @@
 package ref.ex;
 
-public class ProductOrderMain2 {
+import java.util.Scanner;
+
+public class ProductOrderMain3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("입력할 주문의 개수를 입력하세요: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        ProductOrder order1 = createOrder("두부", 2000,2);
-        ProductOrder order2= createOrder("김치", 5000, 1);
-        ProductOrder order3 = createOrder("콜라", 1500,2);
+        ProductOrder[] orders = new ProductOrder[n];
+        for(int i = 0; i < orders.length; i++){
+            System.out.print((i + 1) + "번째 주문 정보를 입력하세요.");
 
-        ProductOrder[] orders = {order1, order2, order3};
+            System.out.print("상품명: ");
+            String productname = scanner.nextLine();
+
+            System.out.print("가격: ");
+            int price = scanner.nextInt();
+
+            System.out.print("수량: ");
+            int quantity = scanner.nextInt();
+            scanner.nextLine();
+            orders[i] = createOrder(productname, price,quantity);
+        }
         printOrders(orders);
         int totalAmount = getTotalOrder(orders);
         System.out.println("총 결제 금액: " + totalAmount);
