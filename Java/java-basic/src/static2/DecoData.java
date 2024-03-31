bffc9f3a2b58965ce145a5bf686f6f4ea04a849a
baechanghyeon12 <qo99033@naver.com>
2024-03-31 16:05:19 +0900

    Java static, final

---
+++ Java/java-basic/src/static2/DecoData.java
@@ -0,0 +1,29 @@
+package static2;
+
+public class DecoData {
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        //instanceValue++;
+        //instanceMethod();
+
+        staticValue++; //정적 변수 접근
+        staticMethod();
+    }
+
+    public void instanceCall() {
+        instanceValue++;
+        instanceMethod();
+
+        staticValue++; //정적 변수 접근
+        staticMethod();
+    }
+    private void instanceMethod() {
+        System.out.println("instanceValue= " + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue= " + staticValue);
+    }
+}
