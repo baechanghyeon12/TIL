3ed8375b9b27958803498d1e17c417a7def52c02
baechanghyeon12 <qo99033@naver.com>
2024-04-09 21:58:11 +0900

    Java modified

--- Java/java-basic/src/poly/ex4/AbstractMain.java
+++ Java/java-basic/src/poly/ex4/AbstractMain.java
@@ -5,7 +5,6 @@ public class AbstractMain {
     public static void main(String[] args) {
         // 추상클래스 생성 불가
         //AbstractAnimal animal = new AbstractMain();
-
         Dog dog = new Dog();
         Cat cat = new Cat();
         Caw caw = new Caw();
@@ -25,7 +24,6 @@ public class AbstractMain {
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
     }
-
     private static void moveAnimal(AbstractAnimal animal) {
         System.out.println("동물 이동 테스트 시작");
         animal.move();
