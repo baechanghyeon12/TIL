c8e932172927c8bbdb6a0557174ab3987787c6de
baechanghyeon12 <qo99033@naver.com>
2024-04-13 21:52:22 +0900

    Java Spring Practice

---
+++ practice-spring/src/main/java/practice/practicespring/SpringConfig.java
@@ -0,0 +1,21 @@
+package practice.practicespring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import practice.practicespring.repository.MemberRepository;
+import practice.practicespring.repository.MemoryMemberRepository;
+import practice.practicespring.service.MemberService;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
