a1374b612ba753e7290fca6669d235a6ba8b01ec
baechanghyeon12 <qo99033@naver.com>
2024-04-14 21:40:59 +0900

    Java Spring JPA,AOP

--- practice-spring/src/main/java/practice/practicespring/SpringConfig.java
+++ practice-spring/src/main/java/practice/practicespring/SpringConfig.java
@@ -1,21 +1,42 @@
 package practice.practicespring;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import practice.practicespring.repository.MemberRepository;
-import practice.practicespring.repository.MemoryMemberRepository;
+import practice.practicespring.aop.TimeTraceAop;
+import practice.practicespring.repository.*;
 import practice.practicespring.service.MemberService;
+
+import javax.sql.DataSource;
+import javax.swing.text.html.parser.Entity;
+import java.sql.Time;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
+
 }
--- practice-spring/src/main/java/practice/practicespring/service/MemberService.java
+++ practice-spring/src/main/java/practice/practicespring/service/MemberService.java
@@ -2,6 +2,7 @@ package practice.practicespring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import practice.practicespring.domain.Member;
 import practice.practicespring.repository.MemberRepository;
 import practice.practicespring.repository.MemoryMemberRepository;
@@ -9,6 +10,7 @@ import practice.practicespring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -19,10 +21,11 @@ public class MemberService {
 
 
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+            //같은 이름이 있는 중복 회원X
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
